2e85f511156aa1b3b1a2b569bc4ecb653d1fe81c
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-05-05 14:23:36 -0500

    fin seccion7

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,7 +21,11 @@ class MyDocument extends Document {
                         rel='stylesheet'
                         href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
                     />
-                    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+                    <link
+                        rel='shortcut icon'
+                        href='/favicon.ico'
+                        type='image/x-icon'
+                    />
                 </Head>
                 <body>
                     <Main />
